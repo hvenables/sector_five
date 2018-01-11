f4249e890fd1c62de778b5f188dedb204a604bbd
Harry Venables <venablesh@yahoo.co.uk>
2018-01-11 19:24:33 +0000

    Added ability to rotate the ship

--- player.rb
+++ player.rb
@@ -4,4 +4,16 @@ class Player
     @angle    = 0
     @image    = Gosu::Image.new('images/ship.png')
   end
+
+  def draw
+    @image.draw_rot(@position[:x], @position[:y], 1, @angle)
+  end
+
+  def turn_right
+    @angle += 3
+  end
+
+  def turn_left
+    @angle -= 3
+  end
 end
--- sector_five.rb
+++ sector_five.rb
@@ -5,6 +5,17 @@ class SectorFive < Gosu::Window
   def initialize
     super(800, 600)
     self.caption = 'Sector Five'
+
+    @player = Player.new(self)
+  end
+
+  def draw
+    @player.draw
+  end
+
+  def update
+    @player.turn_left if button_down?(Gosu::KbLeft) || button_down?(Gosu::KbA)
+    @player.turn_right if button_down?(Gosu::KbRight) || button_down?(Gosu::KbD)
   end
 end
 
